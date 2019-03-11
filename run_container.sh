52d00f5d5f712121f191ae1e19cce9c6e86982fc
jach4 <jecing@qq.com>
2019-03-11 15:23:54 +0800

    []

--- run_container.sh
+++ run_container.sh
@@ -35,37 +35,37 @@ else
     echo -e "\n\n\nWho are you?\n\n0.chem(chem)\n1.胡建星(jhu)\n2.王铎行(hhhh)\n3.赖俊勇(wllg)\n4.夏虓林(ex2l)\n5.杜子腾(streamer)\n6.其他\n"
     read -p "Type in your index and press enter: " IDX_USER
     case $IDX_USER in
-    [0])
+    0)
         P_TENSORBOARD=2206
         P_SSH=2222
         P_JUPYTER=2288
         USER_NAME="chem"
         ;;
-    [1])
+    1)
         P_TENSORBOARD=11106
         P_SSH=11122
         P_JUPYTER=11188
         USER_NAME="jhu"
         ;;
-    [2])
+    2)
         P_TENSORBOARD=17022
         P_SSH=17020
         P_JUPYTER=17021
         USER_NAME="hhhh"
         ;;
-    [3])
+    3)
         P_TENSORBOARD=16022
         P_SSH=16020
         P_JUPYTER=16021
         USER_NAME="wllg"
         ;;
-    [4])
+    4)
         P_TENSORBOARD=18022
         P_SSH=18020
         P_JUPYTER=18021
         USER_NAME="ex2l"
         ;;
-    [5])
+    5)
         P_TENSORBOARD=15022
         P_SSH=15020
         P_JUPYTER=15021
