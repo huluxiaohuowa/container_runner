37936ba1ed76284c26c1932d0d09031dccaaae67
wllllg <39621765+wllllg@users.noreply.github.com>
2019-03-11 15:23:13 +0800

    Update run_container.sh

--- run_container.sh
+++ run_container.sh
@@ -33,7 +33,7 @@ else
 
 
     echo -e "\n\n\nWho are you?\n\n0.chem(chem)\n1.胡建星(jhu)\n2.王铎行(hhhh)\n3.赖俊勇(wllg)\n4.夏虓林(ex2l)\n5.杜子腾(streamer)\n6.其他\n"
-    read -p "Type in your index and press enter: " IDX_USER
+    read -p "Type your index and press enter: " IDX_USER
     case $IDX_USER in
     [0])
         P_TENSORBOARD=2206
