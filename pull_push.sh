512ad9a9dce1574f3e6b403c1cbe1380fde6d48d
jach4 <jecing@qq.com>
2019-03-19 18:04:00 +0800

    fix(bug): k_del refactor(modeule): del img

--- pull_push.sh
+++ pull_push.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #created by jach(4@jach.vip)
 
-YES=y
 
-read -p "Do you want to delete this image after pushing it to the regestry?(y/n)" $K_DEL
+
+read -p "Do you want to delete this image after pushing it to the regestry?(y/n)" K_DEL
 if [ $# -eq 0 ]
 then
     echo -e "\n\nUsage: ppp image_version"
@@ -16,6 +16,17 @@ fi
 docker pull jecing/tf20:$VERSION
 docker tag jecing/tf20:$VERSION 192.168.1.141:5000/jecing/tf20:$VERSION
 docker push 192.168.1.141:5000/jecing/tf20:$VERSION
-if [ "$K_DEL" == "$YES" ];then
-    docker rmi -f jecing/tf20:$VERSION
-fi
+# if [ "$K_DEL" == "$YES" ];then
+#     docker rmi -f jecing/tf20:$VERSION
+# fi
+
+case $K_DEL in
+    [yY][eE][sS]|[yY])
+        docker rmi -f jecing/tf20:$VERSION
+        docker rmi -f 192.168.1.141:5000/jecing/tf20:$VERSION
+        ;;
+    [nN][oO]|[nN])
+        echo "FUCK!"
+        ;;
+    *)
+esac
