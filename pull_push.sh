37f244d4b595e2fd0d7390a77d0f30dced57c806
jach4 <jecing@qq.com>
2019-03-19 15:33:47 +0800

    fix(bus): space in if

--- pull_push.sh
+++ pull_push.sh
@@ -10,9 +10,10 @@ then
     echo "e.g. >> $ pp 1.0.7"
     read -p "Or you can input your version: " VERSION
 else
-    VERSION=$1    
+    VERSION=$1
 fi
+
 `docker pull jecing/tf20:$VERSION && docker tag jecing/tf20:$VERSION 192.168.1.141:5000/jecing/tf20:$VERSION && docker push 192.168.1.141:5000/jecing/tf20:$VERSION`
-    if [$K_DEL == $YES];then
-        `docker rmi -f jecing/tf20:$VERSION`
-    fi
+if [ "$K_DEL" == "$YES" ];then
+    `docker rmi -f jecing/tf20:$VERSION`
+fi
