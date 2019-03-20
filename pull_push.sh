5eead90571ea8585fa5c97196f749e785841607b
jach4 <jecing@qq.com>
2019-03-20 09:53:54 +0800

    feat(file): newest tag from dockerhub

--- pull_push.sh
+++ pull_push.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 #created by jach(4@jach.vip)
 
-
+LATEST=`wget -q https://registry.hub.docker.com/v1/repositories/jecing/tf20/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' | sort -rV | head -n 1`
 
 read -p "Do you want to delete this image after pushing it to the regestry?(y/n)" K_DEL
+
 if [ $# -eq 0 ]
 then
-    echo -e "\n\nUsage: ppp image_version"
-    echo "e.g. >> $ ppp 1.0.7"
-    read -p "Or you can input your version and press ENTER: " VERSION
+    echo -e "\n\nThe latest version of image is $LATEST\n"
+    echo -e "Usage: ppp image_version"
+    echo "e.g. >> $ sudo ppp $LATEST"
+    read -p "Or you can input your version and press ENTER: " V_IN
 else
-    VERSION=$1
+    V_IN=$1
 fi
+
+case $V_IN in
+$LATEST)
+    VERSION=$LATEST
+    ;;
+*)
+echo -e "\nYour version $V_IN does not match the newest version: $LATEST"
+read -p "Use the latest image version? (y/n): " U_LATEST
+
+    case $U_LATEST in
+        [yY][eE][sS]|[yY])
+            VERSION=$LATEST
+            ;;
+        [nN][oO]|[nN])
+            VERSION=$V_IN
+            ;;
+        *)
+    esac
+esac
+
+
 
 docker pull jecing/tf20:$VERSION
 docker tag jecing/tf20:$VERSION 192.168.1.141:5000/jecing/tf20:$VERSION
