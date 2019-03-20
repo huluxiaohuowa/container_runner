26a18e9278085677d803e85e825f742ef7303d96
jach4 <jecing@qq.com>
2019-03-20 10:31:33 +0800

    fix(bug)

--- del_image.sh
+++ del_image.sh
@@ -3,5 +3,5 @@
 REGISTRY_ROOT=/home/docker/v/registry/docker/registry/v2/repositories
 read -p "Input your images name(e.g. jecing/tf20): " IMG_NAME
 read -p "Input your tag(e.g. 1.0.7): " TAG
-`rm -f $REGISTRY_ROOT/$IMG_NAME/_manifests/tags/$TAG`
+`rm -rf $REGISTRY_ROOT/$IMG_NAME/_manifests/tags/$TAG`
 `docker exec -it registry /bin/registry garbage-collect  /etc/docker/registry/config.yml`
