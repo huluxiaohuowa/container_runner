f1d614f3acf9b92c1a826100f29a021528f2d54a
jach4 <jecing@qq.com>
2019-04-14 18:24:03 +0800

    la

--- run_container.sh
+++ run_container.sh
@@ -48,42 +48,42 @@ case $IDX_USER in
     P_TENSORBOARD=2206
     P_SSH=2222
     P_JUPYTER=2288
-    # P_PCJ=2289
+    P_CON=2289
     USER_NAME="chem"
     ;;
 1)
     P_TENSORBOARD=10402
     P_SSH=10400
     P_JUPYTER=10401
-    # P_PCJ=10405
+    P_CON=10403
     USER_NAME="jhu"
     ;;
 2)
     P_TENSORBOARD=17022
     P_SSH=17020
     P_JUPYTER=17021
-    # P_PCJ=17023
+    P_CON=17023
     USER_NAME="hhhh"
     ;;
 3)
     P_TENSORBOARD=16022
     P_SSH=16020
     P_JUPYTER=16021
-    # P_PCJ=16023
+    P_CON=16023
     USER_NAME="wllg"
     ;;
 4)
     P_TENSORBOARD=18022
     P_SSH=18020
     P_JUPYTER=18021
-    # P_PCJ=18023
+    P_CON=18023
     USER_NAME="ex2l"
     ;;
 5)
     P_TENSORBOARD=15022
     P_SSH=15020
     P_JUPYTER=15021
-    # P_PCJ=15023
+    P_CON=15023
     USER_NAME="streamer"
     ;;    
 *)
@@ -101,10 +101,10 @@ NUM_CON=`docker ps -a --format "{{.Names}}" | grep -w $CON -c`
 
 case $SEEN in
     [yY][eE][sS]|[yY])
-        RUN="nvidia-docker run -d --pid=host --restart=always -v /home/docker/v/$USER_NAME:/root/jupyter -v /etc/localtime:/etc/localtime:ro -p $P_TENSORBOARD:6006  -p $P_SSH:22 -p $P_JUPYTER:8888 --name $USER_NAME -h $USER_NAME-$HOST 192.168.1.141:5000/jecing/tf20:$VERSION /entrypoint.sh"
+        RUN="nvidia-docker run -d --pid=host --restart=always -v /home/docker/v/$USER_NAME:/root/jupyter -v /etc/localtime:/etc/localtime:ro -p $P_TENSORBOARD:6006 -p $P_CON:9999 -p $P_SSH:22 -p $P_JUPYTER:8888 --name $USER_NAME -h $USER_NAME-$HOST 192.168.1.141:5000/jecing/tf20:$VERSION /entrypoint.sh"
         ;;
     [nN][oO]|[nN])
-        RUN="nvidia-docker run -d --restart=always -v /home/docker/v/$USER_NAME:/root/jupyter -v /etc/localtime:/etc/localtime:ro -p $P_TENSORBOARD:6006  -p $P_SSH:22 -p $P_JUPYTER:8888 --name $USER_NAME -h $USER_NAME-$HOST 192.168.1.141:5000/jecing/tf20:$VERSION /entrypoint.sh"
+        RUN="nvidia-docker run -d --restart=always -v /home/docker/v/$USER_NAME:/root/jupyter -v /etc/localtime:/etc/localtime:ro -p $P_TENSORBOARD:6006 -p $P_CON:9999 -p $P_SSH:22 -p $P_JUPYTER:8888 --name $USER_NAME -h $USER_NAME-$HOST 192.168.1.141:5000/jecing/tf20:$VERSION /entrypoint.sh"
         ;;
     *)
 esac
