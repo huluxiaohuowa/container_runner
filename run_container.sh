b323bf2b1b7b8c68c82601bf4ca4462112ecbebd
jach4 <jecing@qq.com>
2019-04-23 20:36:35 +0800

    feat(ports): remove tensorboard ports

--- run_container.sh
+++ run_container.sh
@@ -45,52 +45,53 @@ echo -e "\n\n\nWho are you?\n\n0.chem(chem)\n1.胡建星(jhu)\n2.王铎行(hhhh)
 read -p "Type your index and press enter: " IDX_USER
 case $IDX_USER in
 0)
-    P_TENSORBOARD=2206
+    P_CON=2206
     P_SSH=2222
     P_JUPYTER=2288
-    P_CON=2289
+    # P_CON=2289
     USER_NAME="chem"
     ;;
 1)
-    P_TENSORBOARD=10402
+    P_CON=10402
     P_SSH=10400
     P_JUPYTER=10401
-    P_CON=10405
+    # P_CON=10405
     USER_NAME="jhu"
     ;;
 2)
-    P_TENSORBOARD=17022
+    P_CON=17022
     P_SSH=17020
     P_JUPYTER=17021
-    P_CON=17023
+    # P_CON=17023
     USER_NAME="hhhh"
     ;;
 3)
-    P_TENSORBOARD=16022
+    P_CON=16022
     P_SSH=16020
     P_JUPYTER=16021
-    P_CON=16023
+    # P_CON=16023
     USER_NAME="wllg"
     ;;
 4)
-    P_TENSORBOARD=18022
+    P_CON=18022
     P_SSH=18020
     P_JUPYTER=18021
-    P_CON=18023
+    # P_CON=18023
     USER_NAME="ex2l"
     ;;
 5)
-    P_TENSORBOARD=15022
+    P_CON=15022
     P_SSH=15020
     P_JUPYTER=15021
-    P_CON=15023
+    # P_CON=15023
     USER_NAME="streamer"
     ;;    
 *)
 read -p "Type in your container name and press enter: " USER_NAME
 read -p "Type in your SSH port in this host and press enter: " P_SSH
 read -p "Type in your Jupyter port in this host and press enter: " P_JUPYTER
-read -p "Type in your Tensorboard port in this host and press enter: " P_TENSORBOARD
+read -p "Type in your anaconda port in this host and press enter: " P_CON
+# read -p "Type in your Tensorboard port in this host and press enter: " P_CON
 # read -p "Type in your PyCharm Jupyter port in this host and press enter: " P_PCJ
 esac
 
@@ -101,15 +102,15 @@ NUM_CON=`docker ps -a --format "{{.Names}}" | grep -w $CON -c`
 
 case $SEEN in
     [yY][eE][sS]|[yY])
-        RUN="nvidia-docker run -d --pid=host --restart=always -v /home/docker/v/$USER_NAME:/root/jupyter -v /etc/localtime:/etc/localtime:ro -p $P_TENSORBOARD:6006 -p $P_CON:9999 -p $P_SSH:22 -p $P_JUPYTER:8888 --name $USER_NAME -h $USER_NAME-$HOST 192.168.1.141:5000/jecing/tf20:$VERSION /entrypoint.sh"
+        RUN="nvidia-docker run -d --pid=host --restart=always -v /home/docker/v/$USER_NAME:/root/jupyter -v /etc/localtime:/etc/localtime:ro -p $P_CON:9999 -p $P_SSH:22 -p $P_JUPYTER:8888 --name $USER_NAME -h $USER_NAME-$HOST 192.168.1.141:5000/jecing/tf20:$VERSION /entrypoint.sh"
         ;;
     [nN][oO]|[nN])
-        RUN="nvidia-docker run -d --restart=always -v /home/docker/v/$USER_NAME:/root/jupyter -v /etc/localtime:/etc/localtime:ro -p $P_TENSORBOARD:6006 -p $P_CON:9999 -p $P_SSH:22 -p $P_JUPYTER:8888 --name $USER_NAME -h $USER_NAME-$HOST 192.168.1.141:5000/jecing/tf20:$VERSION /entrypoint.sh"
+        RUN="nvidia-docker run -d --restart=always -v /home/docker/v/$USER_NAME:/root/jupyter -v /etc/localtime:/etc/localtime:ro -p $P_CON:9999 -p $P_SSH:22 -p $P_JUPYTER:8888 --name $USER_NAME -h $USER_NAME-$HOST 192.168.1.141:5000/jecing/tf20:$VERSION /entrypoint.sh"
         ;;
     *)
 esac
 
-# RUN="nvidia-docker run -d --restart=always -v /home/docker/v/$USER_NAME:/root/jupyter -v /etc/localtime:/etc/localtime:ro -p $P_TENSORBOARD:6006  -p $P_SSH:22 -p $P_JUPYTER:8888 --name $USER_NAME -h $USER_NAME-$HOST 192.168.1.141:5000/jecing/tf20:$VERSION /entrypoint.sh"
+# RUN="nvidia-docker run -d --restart=always -v /home/docker/v/$USER_NAME:/root/jupyter -v /etc/localtime:/etc/localtime:ro -p $P_CON:6006  -p $P_SSH:22 -p $P_JUPYTER:8888 --name $USER_NAME -h $USER_NAME-$HOST 192.168.1.141:5000/jecing/tf20:$VERSION /entrypoint.sh"
 
 if [ $NUM_CON -ne 0 ]
 then
