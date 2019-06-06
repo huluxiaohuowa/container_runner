e1738524b9eb2e3c1bf1faa13fcf135f7f654bca
jach4 <jecing@qq.com>
2019-06-06 16:06:30 +0800

    啊

--- catcon.sh
+++ catcon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #created by jach(4@jach.vip)
 
-containerid=`cat /proc/$/cgroup | head -n 1 | cut -d "/" -f3`
+containerid=`cat /proc/$1/cgroup | head -n 1 | cut -d "/" -f3`
 docker inspect --format '{{.Name}}' "${containerid}" | sed 's/^\///'
 
