77eebbb291e5788593e0e9ddb19dcc398b01e172
huluxiaohuowa <jianxin.hu@ictrek.com>
2024-05-08 07:38:08 +0000

    fix: 🐛 fix catcon

--- catcon.sh
+++ catcon.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 #created by jach(4@jach.vip)
 
-containerid=`cat /proc/$1/cgroup | head -n 1 | cut -d "/" -f3`
-docker inspect --format '{{.Name}}' "${containerid}" | sed 's/^\///'
-
+docker ps --no-trunc | grep $(cat /proc/$1/cgroup | grep -oE '[0-9a-f]{64}' | head -1) | sed 's/^.* //'
