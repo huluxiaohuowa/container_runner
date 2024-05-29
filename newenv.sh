caaf7bb62e67e4707491b942eb62be18215415c1
huluxiaohuowa <jianxin.hu@ictrek.com>
2024-05-29 04:43:37 +0000

    fix: 🐛 some fixes

--- newenv.sh
+++ newenv.sh
@@ -2,7 +2,7 @@
 read -p "Input your env name and press ENTER: " ENV
 read -p "Input your python version of this environment and press ENTER: " PYVER
 
-mamba create --prefix=/home/jhu/dev/envs/$ENV python=$PYVER -y
+mamba create --prefix=~/dev/envs/$ENV python=$PYVER -y
 
 mamba activate $ENV
 pip install ipykernel
--- rmenv.sh
+++ rmenv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 read -p "Input your env name and press ENTER: " ENV
-mamba remove --prefix=/home/jhu/dev/envs/$ENV --all -y
+mamba remove --prefix=~/dev/envs/$ENV --all -y
 jupyter kernelspec uninstall $ENV -y
 jupyter kernelspec list
